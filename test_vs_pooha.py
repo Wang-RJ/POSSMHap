f90d2196f41e6e33a230435dac0b25853fd464fd
nguyetrt <nguyetrt@iu.edu>
2024-11-21 23:02:56 -0500

    filtered out failed positions in pooha

--- test_vs_pooha.py
+++ test_vs_pooha.py
@@ -1,1 +1,12 @@
 import pandas as pd
+import numpy as np
+
+# Filter out the rows where POOHA failed
+
+original_data = pd.read_csv('SNP_samplingSNP_all_detail.txt', sep='\t', header=None)
+original_data.columns = ["chromosome", "muttype", "position"]
+pooha_data = pd.read_csv('POOHA_failed.txt', sep='\t')
+# print(original_data.shape)
+# print(pooha_data.shape)
+df = pd.merge(original_data, pooha_data, on=["chromosome", "position"], how='inner')
+print(df)
